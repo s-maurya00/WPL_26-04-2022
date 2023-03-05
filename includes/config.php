77a1b69b50ef1f0f7aba2e46bffb35beb730a61a
Sushil <mauryasushil045@gmail.com>
2023-03-05 15:26:03 +0530

    feat: added index.html

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,7 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', '27-04-2022_wpl_real_estate') or die('connection failed');
+    try{
+        $conn = mysqli_connect('localhost', 'root', '', '27-04-2022_wpl_real_estate') or die('connection failed');
+    } catch (Exception $e) {
+        echo "Error occured";
+    }
 ?>
